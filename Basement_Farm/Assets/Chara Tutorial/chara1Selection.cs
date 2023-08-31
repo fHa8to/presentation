b6579871fab979093d6715a2eaa8338eb85ae095
fHa8to <135081859+fHa8to@users.noreply.github.com>
2023-08-31 15:27:32 +0900

    up

--- Basement_Farm/Assets/Chara Tutorial/chara1Selection.cs
+++ Basement_Farm/Assets/Chara Tutorial/chara1Selection.cs
@@ -2,10 +2,6 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SocialPlatforms.GameCenter;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 
 public class chara1Selection : MonoBehaviour
